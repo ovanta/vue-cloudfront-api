8450d88025872db9abb6e3a9b8f6335b804a93f4
Simonwep <toports@gmx.de>
2018-12-25 15:45:33 +0100

    Add delete account

--- src/api/api.js
+++ src/api/api.js
@@ -18,6 +18,7 @@ const update = require('../endpoints/nodes/update.js');
 const login = require('../endpoints/user/login.js');
 const register = require('../endpoints/user/register.js');
 const settings = require('../endpoints/user/settings.js');
+const deleteAccount = require('../endpoints/user/deleteAccount.js');
 
 const download = require('../endpoints/data/download.js');
 const upload = require('../endpoints/data/upload.js');
@@ -31,6 +32,7 @@ const mapHandler = mod => {
         }).catch(reason => {
 
             if (typeof reason !== 'string') {
+                /* eslint-disable no-console */
                 console.warn('Suspicious error', reason);
                 reason = 'Try again later please.';
             }
@@ -52,12 +54,13 @@ api.post('/removeMark', json, mapHandler(removeMark));
 api.post('/rename', json, mapHandler(rename));
 api.post('/update', json, mapHandler(update));
 
-api.post('/login', json, mapHandler(login));
-api.post('/register', json, mapHandler(register));
-api.post('/settings', json, mapHandler(settings));
-
 api.post('/delete', json, mapHandler(del));
 api.post('/upload', mapHandler(upload));
 api.get('/download', download);
+
+api.post('/login', json, mapHandler(login));
+api.post('/register', json, mapHandler(register));
+api.post('/settings', json, mapHandler(settings));
+api.post('/deleteAccount', json, mapHandler(deleteAccount));
 
 module.exports = api;
---
+++ src/endpoints/user/deleteAccount.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+const authViaApiKey = require('../tools/authViaApiKey');
+const config = require('../../../config/config');
+const user = require('../../models/user');
+const node = require('../../models/node');
+
+module.exports = async req => {
+    const {password, apikey} = req.body;
+
+    // Find user and validate color
+    const currentUser = await authViaApiKey(apikey);
+
+    // Validate password
+    if (!bcrypt.compareSync(password, currentUser.password)) {
+        throw  config.errors.user.wrongPassword;
+    }
+
+    return Promise.all([
+
+        // Delete files
+        node.find({owner: currentUser.id}).then(nodes => {
+            for (let i = 0, n; n = nodes[i], i < nodes.length; i++) {
+                if (n.type === 'file') {
+
+                    // Build local storage path and check if file exists
+                    const path = `${__dirname}/../../..${config.storagepath}/${node.id}`;
+                    if (fs.existsSync(path)) {
+                        fs.unlink(path, () => 0);
+                    }
+                }
+            }
+        }),
+
+        // Remove nodes
+        node.deleteMany({owner: currentUser.id}).exec(),
+
+        // Remove user
+        user.deleteOne({id: currentUser.id})
+    ]);
+};
