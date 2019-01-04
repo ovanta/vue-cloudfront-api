12e07a7a3f7efe39ff252fe5c16be2fbd0106f49
Simonwep <toports@gmx.de>
2019-01-04 15:07:06 +0100

    Refactor upload system

--- src/api/endpoints/data/upload.js
+++ src/api/endpoints/data/upload.js
@@ -6,35 +6,46 @@ const config = require('../../../../config/config');
 const nodeModel = require('../../../models/node');
 
 module.exports = async req => {
-    const {apikey, parent} = req.query;
-
-    // Authenticate user
-    const user = await authViaApiKey(apikey);
-
-    // Check if destination exists and is a folder
-    const destNode = await nodeModel.findOne({owner: user.id, id: parent}).exec();
-    if (!destNode || destNode.type !== 'dir') {
-        throw config.errors.invalid.destination;
-    }
 
     // Create formidable instance and set save-path
     const form = new formidable.IncomingForm();
     form.maxFileSize = config.maxRequestSize;
     form.uploadDir = _storagePath;
 
-    return new Promise(async resolve => {
+    return new Promise(async (resolve, reject) => {
 
         // Parse form
-        form.parse(req, (err, fields, files) => {
+        form.parse(req, async (err, fields, files) => {
+            const {apikey} = fields;
 
-            if (err) {
-                return;
+            if (err || !apikey) {
+                return reject('Invalid request');
             }
+
+            // Authenticate user
+            const user = await authViaApiKey(apikey);
 
             // Rename files and create nodes
             const nodes = [];
-            for (const file of Object.values(files)) {
+            for (const [prefixedParent, file] of Object.entries(files)) {
                 const nodeid = uid();
+
+                /**
+                 * Beause fordata cannot have multiple values assigned to
+                 * one key, they're prefixed with a index: [HASH]-[INDEX]
+                 *
+                 * Extract the hash.
+                 */
+                const [parent] = prefixedParent.match(/^[^-]+/) || [];
+                if (!parent) {
+                    return;
+                }
+
+                // Check if destination exists and is a folder
+                const destNode = await nodeModel.findOne({owner: user.id, id: parent}).exec();
+                if (!destNode || destNode.type !== 'dir') {
+                    throw config.errors.invalid.destination;
+                }
 
                 // Rename file
                 const [dir] = file.path.match(/.*[\\/]/);
@@ -56,6 +67,9 @@ module.exports = async req => {
             Promise.all(nodes).then(n => {
                 const filteredNodes = n.map(v => pick(v, ['id', 'parent', 'lastModified', 'type', 'name', 'marked', 'size', 'staticIds']));
                 resolve(filteredNodes);
+            }).catch(() => {
+                // TODO: Log?
+                reject('Internal error');
             });
         });
     });
